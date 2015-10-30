d30d666255881d2e7de650164ec2340785f8b177
David Prueller <david.a.prueller@gmail.com>
2015-10-30 16:48:48 +0100

    testkernel

---
+++ test.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  test
+//
+//  Created by Mac User on 30.10.15.
+//  Copyright © 2015 David Prueller. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+    //THIS IS LOCAL AND NOT ON GIT  
+}
